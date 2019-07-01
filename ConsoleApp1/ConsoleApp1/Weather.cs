9fe991452531ac7050ba1f8093c152cab194c7db
dana <danaw09@gmail.com>
2019-07-01 10:28:32 -0500

    added member variables and some weather functions in order to display current weather information

--- ConsoleApp1/ConsoleApp1/Weather.cs
+++ ConsoleApp1/ConsoleApp1/Weather.cs
@@ -8,5 +8,50 @@ namespace ConsoleApp1
 {
     class Weather
     {
+        private Random rnd;
+        private int weatherOutside;
+        private string descriptionOfWeather;
+
+        public Weather(Random rnd)
+        {
+            this.rnd = rnd;
+            GetTheWeather();
+            descriptionOfWeather = GetTheConditions();
+        }
+
+        public string DescriptionOfWeather
+        {
+            get
+            {
+                return descriptionOfWeather;
+            }
+        }
+
+        public int WeatherOutside
+        {
+            get
+            {
+                return weatherOutside;
+            }
+        }
+
+        private void GetTheWeather()
+        {
+            weatherOutside = rnd.Next(45, 95);
+        }
+
+        public string GetTheConditions()
+        {
+            int result;
+            List<string> conditions = new List<string>();
+            conditions.Add("Sunny");
+            conditions.Add("Cloudy");
+            conditions.Add("Rainy");
+            conditions.Add("Overcast");
+            conditions.Add("Hazy");
+            conditions.Add("Foggy");
+            result = rnd.Next(0, conditions.Count - 1);
+            return conditions[result].ToString();
+        }
     }
 }
