910fc2ac3c5dcfed9acffcf2810b6261f6673c44
dana <danaw09@gmail.com>
2019-07-01 11:04:17 -0500

    added more member variables for customer files

--- ConsoleApp1/ConsoleApp1/day.cs
+++ ConsoleApp1/ConsoleApp1/day.cs
@@ -8,5 +8,13 @@ namespace ConsoleApp1
 {
     class day
     {
+        private string name;
+        private int number;
+        private int totalCustomers;
+        internal Weather weather;
+        internal Report report;
+        public List<customer> customers = new List<customer>();
+        Random rnd = new Random();
+
     }
 }
