b0144027a1dad90cd00e9087520a9778a4b3740d
dana <danaw09@gmail.com>
2019-07-01 14:33:18 -0500

    added ctor to player class and current cash function

--- ConsoleApp1/ConsoleApp1/player.cs
+++ ConsoleApp1/ConsoleApp1/player.cs
@@ -8,18 +8,27 @@ namespace ConsoleApp1
 {
     public class player
     {
-        private int weeklyProfits;
+        private int weeklyCash;
         public inventory inventory;
         public string name;
         public RecipeBook recipeBook;
+        private float currentCash;
+        private int overallCashRd;
 
 
         public player()
         {
             inventory = new inventory();
             recipeBook = new RecipeBook();
+            currentCash = 25.00f;
 
         }
 
+        public string GetUserInfo()
+        {
+            Console.WriteLine("enter name");
+            name = Console.ReadLine();
+            return name;
+        }
 
  }   }
