f47896e9ce303f0e22a41ffea5649a30608547d6
dana <danaw09@gmail.com>
2019-07-01 21:16:34 -0500

    added some member variables and switch cases to weather case

--- ConsoleApp1/ConsoleApp1/Weather.cs
+++ ConsoleApp1/ConsoleApp1/Weather.cs
@@ -8,11 +8,64 @@ namespace ConsoleApp1
 {
    public class Weather
     {
-        private Random rnd;
-        // private int  ;
-        private string  descriptionOfWeather;
+        public string temperature;
+        public string condition;
 
+
+        public int GetTemp(Random rnd)
+        {
+            int temperature = rnd.Next(1, 4);
+
+            switch (temperature)
+            {
+                case 1:
+                    this.temperature = "Hot";
+                    break;
+                case 2:
+                    this.temperature = "Warm";
+                    break;
+                case 3:
+                    this.temperature = "Cold";
+                    break;
+                default:
+                    Console.WriteLine("There was an error getting the report");
+                    break;
+            }
+            return temperature;
+        }
+        public int GetOvercast(Random rnd)
+        {
+            int condition = rnd.Next(1, 4);
+
+            switch (condition)
+            {
+                case 1:
+                    this.condition = "and sunny";
+                    break;
+                case 2:
+                    this.condition = "and cloudy";
+                    break;
+                case 3:
+                    this.condition = "with thunderstorms";
+                    break;
+                default:
+                    Console.WriteLine("There was an error getting the report");
+                    break;
+            }
+            return condition;
+        }
+        public void DisplayCurrentWeather()
+        {
+            Console.WriteLine($"Today's forcast is: {temperature} {condition}\n");
+        }
+        public void DisplayForecast()
+        {
+            Console.WriteLine($"The weather for tomorrow is: {temperature} {condition}\n");
+        }
     }
 
-        
 }
+
+
+        
+
